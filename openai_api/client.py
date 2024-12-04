1e7396fbd12c86554a0d0009f8615dcb123ca838
Nathália Vilas Boas <natvbsousa@gmail.com>
2024-12-04 14:31:38 -0300

    my first commit

---
+++ openai_api/client.py
@@ -0,0 +1,16 @@
+import openai
+
+def get_car_ai_bio(model, brand, year):
+    prompt = f"Me mostre uma descrição de venda para o carro {brand} {model} {year} em apenas 250 caracteres. Fale coisas específicas desse modelo."
+
+    openai.api_key = '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'  # Substitua pela sua chave de API
+
+    response = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        messages=[
+            {"role": "user", "content": prompt}
+        ],
+        max_tokens=1000 
+    )
+    
+    return response['choices'][0]['message']['content']
