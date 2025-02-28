9ec5a2ecbf24436565726576a1c98882320d30c4
Nathaliavb <natvbsousa@gmail.com>
2025-02-28 11:58:23 -0300

    iniciando novo historico

---
+++ openai_api/client.py
@@ -0,0 +1,24 @@
+
+import openai
+from pathlib import Path
+import os
+import environ
+
+# Define o BASE_DIR (ajuste se necessário)
+BASE_DIR = Path(__file__).resolve().parent.parent
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+# Carrega a chave da API do OpenAI a partir do .env
+openai.api_key = env('OPENAI_API_KEY')
+
+def get_car_ai_bio(model, brand, year):
+    prompt = f"Me mostre uma descrição de venda para o carro {brand} {model} {year} em apenas 250 caracteres. Fale coisas específicas desse modelo."
+    
+    response = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        messages=[{"role": "user", "content": prompt}],
+        max_tokens=1000 
+    )
+    
+    return response['choices'][0]['message']['content']
